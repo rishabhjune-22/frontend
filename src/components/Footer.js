4ab8916f2dbc4d401125f8b87017cd5b0d8efe4a
rishabhjune-22 <rishabhsstc@gmail.com>
2024-08-22 15:28:04 +0545

    Initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (  
+        <div class="row bg-warning" >
+            <div class="col-sm-12 d-flex justify-content-center">
+                <footer> &copy; <b> 2024 IConnect </b> </footer>
+            </div>
+        </div>
+    );
+}
+ 
+export default Footer;
