21189a62c171563813b6125b861f90f1c9d61e48
rishabhjune-22 <147823699+rishabhjune-22@users.noreply.github.com>
2024-08-26 18:41:48 +0545

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
                 <Link to="/" >   <img className=" w-25"  src="/imgs/logo.png" alt="logo here"/> </Link>
             </div>
             <div className="col-sm-5">
-                <b>   <i> Creating Happy Home Owners Everyday </i> </b>
+                <b>   <i> Happy Home Owners Everyday </i> </b>
             </div>
             <div className="col-sm-3">
             {
